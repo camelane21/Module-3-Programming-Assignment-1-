f2b885ce8ed36fb71a8253d4211a8174520e6310
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-27 04:06:56 -0400

    Update Circle.java
    change

--- Circle.java
+++ Circle.java
@@ -24,3 +24,13 @@ public class Circle extends GeometricObject implements Comparable<Circle> {
     public void setRadius(double radius) {
         this.radius = radius;
     }
+
+     @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
