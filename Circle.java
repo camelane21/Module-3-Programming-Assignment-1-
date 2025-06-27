a9d12365b5162ba5f88c2f15f96ec7bbacdda45c
camelane21 <157440747+camelane21@users.noreply.github.com>
2025-06-27 03:52:10 -0400

    Create Circle.java
    change

---
+++ Circle.java
@@ -0,0 +1,17 @@
+public class Circle extends GeometricObject implements Comparable<Circle> {
+    private double radius;
+
+    public Circle() {
+        super();
+    }
+
+    public Circle(double radius) {
+        super();
+        this.radius = radius;
+    }
+
+    public Circle(double radius, String color, boolean filled) {
+        super(color, filled);
+        this.radius = radius;
+    }
+}
